d84dfc72d5a8f6adbbfaa70949bec0dba6ce33f4
Jens Renders <Renders.jens@gmail.com>
2023-01-02 11:28:44 +0100

    Update operators_affine to the new api

--- imwip/operators/operators_affine.py
+++ imwip/operators/operators_affine.py
@@ -1,31 +1,14 @@
 """
 :file:      operators_affine.py
-:brief:     Affine warping operators. These operators provide
-            high level acces to the warping algorithms of ImWIP.
-:date:      20 DEC 2021
+:brief:     High level access to the affine warping functions through linear operators.
+            These operators also help with differentiation towards affine and rigid parameters.
 :author:    Jens Renders
-            imec-Visionlab
-            University of Antwerp
-            jens.renders@uantwerpen.be
 """
 
 import numpy as np
+import imwip
 from scipy.sparse.linalg import LinearOperator
 from collections.abc import Iterable
-try:
-    from imwip_cuda import (affine_warp_2D,
-                            affine_warp_3D,
-                            adjoint_affine_warp_2D,
-                            adjoint_affine_warp_3D,
-                            grad_affine_warp_2D,
-                            grad_affine_warp_3D)
-except ModuleNotFoundError:
-    from imwip.numba import (affine_warp_2D,
-                             affine_warp_3D,
-                             adjoint_affine_warp_2D,
-                             adjoint_affine_warp_3D,
-                             grad_affine_warp_2D,
-                             grad_affine_warp_3D)
 
 
 class AffineWarpingOperator2D(LinearOperator):
@@ -38,10 +21,11 @@ class AffineWarpingOperator2D(LinearOperator):
             rotation=None,
             translation=None,
             centered=True,
+            degree=3,
             adjoint_type="exact",
             derivative_type="exact",
-            degree=3,
-            indexing="ij"
+            indexing="ij",
+            backend=None
         ):
         self.im_shape = im_shape
         self.im_size = im_shape[0]*im_shape[1]
@@ -79,25 +63,27 @@ class AffineWarpingOperator2D(LinearOperator):
             self.b = center - self.A @ center + self.b
 
         self.shape = (self.im_size, self.im_size)
+        self.degree = degree
         self.adjoint_type = adjoint_type
         self.derivative_type = derivative_type
-        self.degree = degree
         if indexing not in ["ij", "xy"]:
             raise ValueError('indexing should be "xy" or "ij"')
         self.indexing = indexing
+        self.backend = backend
 
     def _matvec(self, x):
 
         # we expect the input as flattened array, so we reshape it
         x = x.reshape(self.im_shape)
 
-        # preform the warp
-        x_warped = affine_warp_2D(
+        # perform the warp
+        x_warped = imwip.affine_warp(
             x,
             self.A,
             self.b,
+            indexing=self.indexing,
             degree=self.degree,
-            indexing=self.indexing
+            backend=self.backend
         )
 
         # return as flattened array
@@ -108,77 +94,82 @@ class AffineWarpingOperator2D(LinearOperator):
         # we expect the input as flattened array, so we reshape it
         x_warped = x_warped.reshape(self.im_shape)
 
-        # preform the adjoint warp
+        # perform the adjoint warp
         if self.adjoint_type == "exact":
-            x = adjoint_affine_warp_2D(
+            x = imwip.adjoint_affine_warp(
                 x_warped,
                 self.A,
                 self.b,
                 degree=self.degree,
-                indexing=self.indexing
+                indexing=self.indexing,
+                backend=self.backend
             )
         elif self.adjoint_type == "inverse":
             Ai = np.linalg.inv(self.A)
-            x = affine_warp_2D(
+            x = imwip.affine_warp(
                 x_warped,
                 Ai,
                 -Ai @ self.b,
                 degree=self.degree,
-                indexing=self.indexing)
+                indexing=self.indexing,
+                backend=self.backend
+            )
         else:
             raise NotImplementedError("adjoint type should be 'exact' or 'inverse'")
                
         # return as flattened array
         return x.ravel()
 
-    def _derivative_scale(self, x, grad_x, grad_y, co_x, co_y):
+    def _derivative_scale(self, x, diff_x, diff_y, co_x, co_y):
         dxds = co_x.astype(np.float32)
         dyds = co_y.astype(np.float32)
         if self.centered:
             dxds -= self.im_shape[0]/2
             dyds -= self.im_shape[1]/2
         
-        grad_sx = grad_x * dxds
-        grad_sy = grad_y * dyds
-        return np.vstack([grad_sx.ravel(), grad_sy.ravel()]).T
+        diff_sx = diff_x * dxds
+        diff_sy = diff_y * dyds
+        return np.vstack([diff_sx.ravel(), diff_sy.ravel()]).T
 
-    def _derivative_zoom(self, x, grad_x, grad_y, co_x, co_y):
-        grad_sx, grad_sy = self._derivative_scale(x, grad_x, grad_y, co_x, co_y).T
-        grad_s = grad_sx + grad_sy
-        return grad_s.reshape((-1, 1))
+    def _derivative_zoom(self, x, diff_x, diff_y, co_x, co_y):
+        diff_sx, diff_sy = self._derivative_scale(x, diff_x, diff_y, co_x, co_y).T
+        diff_s = diff_sx + diff_sy
+        return diff_s.reshape((-1, 1))
 
-    def _derivative_rotation(self, x, grad_x, grad_y, co_x, co_y):
+    def _derivative_rotation(self, x, diff_x, diff_y, co_x, co_y):
         dxdr = -co_x*np.sin(self.rotation) + co_y*np.cos(self.rotation)
         dydr = -co_x*np.cos(self.rotation) - co_y*np.sin(self.rotation)
         if self.centered:
             dxdr += self.im_shape[0]/2*np.sin(self.rotation) - self.im_shape[1]/2*np.cos(self.rotation)
             dydr += self.im_shape[0]/2*np.cos(self.rotation) + self.im_shape[1]/2*np.sin(self.rotation)
 
-        grad_rotation = grad_x*dxdr + grad_y*dydr
-        return grad_rotation.reshape((-1, 1))
+        diff_rotation = diff_x*dxdr + diff_y*dydr
+        return diff_rotation.reshape((-1, 1))
 
-    def _derivative_translation(self, x, grad_x, grad_y):
-        return np.vstack([grad_x.ravel(), grad_y.ravel()]).T
+    def _derivative_translation(self, x, diff_x, diff_y):
+        return np.vstack([diff_x.ravel(), diff_y.ravel()]).T
 
     def derivative(self, x, to=["b"]):
 
         # we expect the input as flattened array, so we reshape it
         x = x.reshape(self.im_shape)
         if self.derivative_type == "exact":
-            grad_x, grad_y = grad_affine_warp_2D(
+            diff_x, diff_y = imwip.diff_affine_warp(
                 x,
                 self.A,
                 self.b,
-                indexing=self.indexing
+                indexing=self.indexing,
+                backend=self.backend
             )
         else:
-            x_warped = affine_warp_2D(
+            x_warped = imwip.affine_warp(
                 x,
                 self.A,
                 self.b,
-                indexing=self.indexing
+                indexing=self.indexing,
+                backend=self.backend
             )
-            grad_x, grad_y = np.gradient(x_warped)
+            diff_x, diff_y = np.gradient(x_warped)
 
         if "rotation" in to or "rot" in to or "scale" in to or "zoom" in to:
             co_x, co_y = np.meshgrid(
@@ -189,13 +180,13 @@ class AffineWarpingOperator2D(LinearOperator):
         derivatives = []
         for var in to:
             if var == "scale":
-                derivatives.append(self._derivative_scale(x, grad_x, grad_y, co_x, co_y))
+                derivatives.append(self._derivative_scale(x, diff_x, diff_y, co_x, co_y))
             elif var == "zoom":
-                derivatives.append(self._derivative_zoom(x, grad_x, grad_y, co_x, co_y))
+                derivatives.append(self._derivative_zoom(x, diff_x, diff_y, co_x, co_y))
             elif var in ["rot", "rotation"]:
-                derivatives.append(self._derivative_rotation(x, grad_x, grad_y, co_x, co_y))
+                derivatives.append(self._derivative_rotation(x, diff_x, diff_y, co_x, co_y))
             elif var in ["trans", "translation", "b"]:
-                derivatives.append(self._derivative_translation(x, grad_x, grad_y))
+                derivatives.append(self._derivative_translation(x, diff_x, diff_y))
             else:
                 derivatives.append(np.zeros((x.size, 0)))
         return derivatives
@@ -213,8 +204,8 @@ class AffineWarpingOperator3D(LinearOperator):
             translation=None,
             centered=True,
             center=None,
-            adjoint_type="exact",
             degree=3,
+            adjoint_type="exact",
             indexing="ij"
         ):
         self.im_shape = im_shape
@@ -282,24 +273,26 @@ class AffineWarpingOperator3D(LinearOperator):
             self.b = center - self.A @ center + self.b
 
         self.shape = (self.im_size, self.im_size)
-        self.adjoint_type = adjoint_type
         self.degree = degree
+        self.adjoint_type = adjoint_type
         if indexing not in ["ij", "xy"]:
             raise ValueError('indexing should be "xy" or "ij"')
         self.indexing = indexing
+        self.backend = backend
 
     def _matvec(self, x):
 
         # we expect the input as flattened array, so we reshape it
         x = x.reshape(self.im_shape)
 
-        # preform the warp
-        x_warped = affine_warp_3D(
+        # perform the warp
+        x_warped = imwip.affine_warp(
             x,
             self.A,
             self.b,
             degree=self.degree,
-            indexing=self.indexing
+            indexing=self.indexing,
+            backend=self.backend
         )
 
         # return as flattened array
@@ -310,13 +303,14 @@ class AffineWarpingOperator3D(LinearOperator):
         # we expect the input as flattened array, so we reshape it
         x_warped = x_warped.reshape(self.im_shape)
 
-        # preform the adjoint warp
-        x = adjoint_affine_warp_3D(
+        # perform the adjoint warp
+        x = imwip.adjoint_affine_warp(
             x_warped,
             self.A,
             self.b,
             degree=self.degree,
-            indexing=self.indexing
+            indexing=self.indexing,
+            backend=self.backend
         )
         
         # return as flattened array
@@ -325,9 +319,9 @@ class AffineWarpingOperator3D(LinearOperator):
     def _derivative_scale(
             self,
             x,
-            grad_x,
-            grad_y,
-            grad_z,
+            diff_x,
+            diff_y,
+            diff_z,
             co_x,
             co_y,
             co_z
@@ -340,31 +334,31 @@ class AffineWarpingOperator3D(LinearOperator):
             dyds -= self.center[1]
             dzds -= self.center[2]
         
-        grad_sx = grad_x * dxds
-        grad_sy = grad_y * dyds
-        grad_sz = grad_z * dzds
-        return np.vstack([grad_sx.ravel(), grad_sy.ravel(), grad_sz.ravel()]).T
+        diff_sx = diff_x * dxds
+        diff_sy = diff_y * dyds
+        diff_sz = diff_z * dzds
+        return np.vstack([diff_sx.ravel(), diff_sy.ravel(), diff_sz.ravel()]).T
 
     def _derivative_zoom(
             self,
             x,
-            grad_x,
-            grad_y,
-            grad_z,
+            diff_x,
+            diff_y,
+            diff_z,
             co_x,
             co_y,
             co_z
         ):
-        grad_sx, grad_sy, grad_sz = self._derivative_scale(x, grad_x, grad_y, co_x, co_y).T
-        grad_s = grad_sx + grad_sy + grad_sz
-        return grad_s.reshape((-1, 1))
+        diff_sx, diff_sy, diff_sz = self._derivative_scale(x, diff_x, diff_y, co_x, co_y).T
+        diff_s = diff_sx + diff_sy + diff_sz
+        return diff_s.reshape((-1, 1))
 
     def _derivative_rotation(
             self,
             x,
-            grad_x,
-            grad_y,
-            grad_z,
+            diff_x,
+            diff_y,
+            diff_z,
             co_x,
             co_y,
             co_z
@@ -406,20 +400,20 @@ class AffineWarpingOperator3D(LinearOperator):
             drj -= rot_k @ (d_rot_j @ (rot_i @ center))
             drk -= d_rot_k @ (rot_j @ (rot_i @ center))
         
-        grad_x = grad_x.ravel()
-        grad_y = grad_y.ravel()
-        grad_z = grad_z.ravel()
-        grad_ri = grad_x*dri[0] + grad_y*dri[1] + grad_z*dri[2]
-        grad_rj = grad_x*drj[0] + grad_y*drj[1] + grad_z*drj[2]
-        grad_rk = grad_x*drk[0] + grad_y*drk[1] + grad_z*drk[2]
-        return np.vstack([grad_ri, grad_rj, grad_rk]).T
+        diff_x = diff_x.ravel()
+        diff_y = diff_y.ravel()
+        diff_z = diff_z.ravel()
+        diff_ri = diff_x*dri[0] + diff_y*dri[1] + diff_z*dri[2]
+        diff_rj = diff_x*drj[0] + diff_y*drj[1] + diff_z*drj[2]
+        diff_rk = diff_x*drk[0] + diff_y*drk[1] + diff_z*drk[2]
+        return np.vstack([diff_ri, diff_rj, diff_rk]).T
 
     def _derivative_cayley(
             self,
             x,
-            grad_x,
-            grad_y,
-            grad_z,
+            diff_x,
+            diff_y,
+            diff_z,
             co_x,
             co_y,
             co_z
@@ -475,26 +469,27 @@ class AffineWarpingOperator3D(LinearOperator):
             dv -= dA_dv @ center
             dw -= dA_dw @ center
         
-        grad_x = grad_x.ravel()
-        grad_y = grad_y.ravel()
-        grad_z = grad_z.ravel()
-        grad_u = grad_x*du[0] + grad_y*du[1] + grad_z*du[2]
-        grad_v = grad_x*dv[0] + grad_y*dv[1] + grad_z*dv[2]
-        grad_w = grad_x*dw[0] + grad_y*dw[1] + grad_z*dw[2]
-        return np.vstack([grad_u, grad_v, grad_w]).T
+        diff_x = diff_x.ravel()
+        diff_y = diff_y.ravel()
+        diff_z = diff_z.ravel()
+        diff_u = diff_x*du[0] + diff_y*du[1] + diff_z*du[2]
+        diff_v = diff_x*dv[0] + diff_y*dv[1] + diff_z*dv[2]
+        diff_w = diff_x*dw[0] + diff_y*dw[1] + diff_z*dw[2]
+        return np.vstack([diff_u, diff_v, diff_w]).T
 
-    def _derivative_translation(self, x, grad_x, grad_y, grad_z):
-        return np.vstack([grad_x.ravel(), grad_y.ravel(), grad_z.ravel()]).T
+    def _derivative_translation(self, x, diff_x, diff_y, diff_z):
+        return np.vstack([diff_x.ravel(), diff_y.ravel(), diff_z.ravel()]).T
 
     def derivative(self, x, to=["b"]):
 
         # we expect the input as flattened array, so we reshape it
         x = x.reshape(self.im_shape)
-        grad_x, grad_y, grad_z = grad_affine_warp_3D(
+        diff_x, diff_y, diff_z = imwip.diff_affine_warp(
             x,
             self.A,
             self.b,
-            indexing=self.indexing
+            indexing=self.indexing,
+            backend=self.backend
         )
 
         if "cayley" in to or "rotation" in to or "rot" in to or "scale" in to or "zoom" in to:
@@ -514,9 +509,9 @@ class AffineWarpingOperator3D(LinearOperator):
             if var == "scale":
                 derivatives.append(self._derivative_scale(
                     x,
-                    grad_x,
-                    grad_y,
-                    grad_z,
+                    diff_x,
+                    diff_y,
+                    diff_z,
                     co_x,
                     co_y,
                     co_z
@@ -524,9 +519,9 @@ class AffineWarpingOperator3D(LinearOperator):
             elif var == "zoom":
                 derivatives.append(self._derivative_zoom(
                     x,
-                    grad_x,
-                    grad_y,
-                    grad_z,
+                    diff_x,
+                    diff_y,
+                    diff_z,
                     co_x,
                     co_y,
                     co_z
@@ -534,9 +529,9 @@ class AffineWarpingOperator3D(LinearOperator):
             elif var in ["rot", "rotation"]:
                 derivatives.append(self._derivative_rotation(
                     x,
-                    grad_x,
-                    grad_y,
-                    grad_z,
+                    diff_x,
+                    diff_y,
+                    diff_z,
                     co_x,
                     co_y,
                     co_z
@@ -544,9 +539,9 @@ class AffineWarpingOperator3D(LinearOperator):
             elif var == "cayley":
                 derivatives.append(self._derivative_cayley(
                     x,
-                    grad_x,
-                    grad_y,
-                    grad_z,
+                    diff_x,
+                    diff_y,
+                    diff_z,
                     co_x,
                     co_y,
                     co_z
@@ -554,9 +549,9 @@ class AffineWarpingOperator3D(LinearOperator):
             elif var in ["trans", "translation", "b"]:
                 derivatives.append(self._derivative_translation(
                     x,
-                    grad_x,
-                    grad_y,
-                    grad_z
+                    diff_x,
+                    diff_y,
+                    diff_z
                 ))
             else:
                 derivatives.append(np.zeros((x.size, 0)))
