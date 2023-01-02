97c3be2e2e22829d235aa6a2c775f20d33e5e2a7
Jens Renders <Renders.jens@gmail.com>
2023-01-02 19:20:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sys
+import os
 sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'ImWIP'
