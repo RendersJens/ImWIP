81a090202eaba74f4666a98edda8627c7950803e
Jens Renders <Renders.jens@gmail.com>
2023-01-02 19:19:51 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,8 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'ImWIP'
 copyright = '2022, Jens Renders'
