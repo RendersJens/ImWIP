1f3e184be21b976d5990c32b087d501b28ec36e9
Jens Renders <Renders.jens@gmail.com>
2023-01-02 20:53:34 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,4 +34,3 @@ autodoc_member_order = 'bysource'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
