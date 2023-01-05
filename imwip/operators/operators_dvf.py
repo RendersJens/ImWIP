681b5f09446b6dc63b97898e29739464502b93a9
Jens Renders <Renders.jens@gmail.com>
2023-01-05 10:33:38 +0100

    Added licence notice to each source file

--- imwip/operators/operators_dvf.py
+++ imwip/operators/operators_dvf.py
@@ -4,6 +4,18 @@
 :author:    Jens Renders
 """
 
+# This file is part of ImWIP.
+#
+# ImWIP is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# ImWIP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of
+# the GNU General Public License along with ImWIP. If not, see <https://www.gnu.org/licenses/>.
 
 import numpy as np
 import imwip
